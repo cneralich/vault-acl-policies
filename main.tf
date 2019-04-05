b47abf3eb70855272c795b4a5dc2e300ef2a2611
cneralich <corrigan@hashicorp.com>
2019-04-05 07:36:08 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,20 @@
+# ADMIN PERMISSIONS
 resource "vault_policy" "admin" {
   name = "TERRAFORM-admin"
 
   policy = "${file("./policy-ADMIN.hcl")}"
 }
 
+# PROVISIONER PERMISSIONS (TERRAFORM)
 resource "vault_policy" "provisioner" {
   name = "TERRAFORM-provisioner"
 
   policy = "${file("./policy-PROVISIONER.hcl")}"
 }
 
+# DEV TEAM PERMISSIONS
 resource "vault_policy" "dev-team" {
   name = "TERRAFORM-dev-team"
 
   policy = "${file("./policy-DEV-TEAM.hcl")}"
 }
-
