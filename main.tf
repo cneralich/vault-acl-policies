67413afe08085fbb26a272caeffe3d44fd7ce8c9
cneralich <corrigan@hashicorp.com>
2019-04-04 19:10:51 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,5 @@
 resource "vault_policy" "example" {
   name = "dev-team"
 
-  #policy = "${file("./example.policy")}"
-  policy = <<EOT
-path "secret/my_app" {
-  policy = "write"
-}
-EOT
+  policy = "${file("./example.policy")}"
 }
