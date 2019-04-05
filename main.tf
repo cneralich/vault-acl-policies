2b2a361de0a95687b2131527cb8fd902a9d917ef
cneralich <corrigan@hashicorp.com>
2019-04-05 06:36:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,18 @@
+resource "vault_policy" "admin" {
+  name = "admin"
+
+  policy = "${file("./policy-ADMIN.hcl")}"
+}
+
+resource "vault_policy" "provisioner" {
+  name = "provisioner"
+
+  policy = "${file("./policy-PROVISIONER.hcl")}"
+}
+
 resource "vault_policy" "dev-team" {
   name = "dev-team"
 
-  policy = "${file("./dev-team.policy")}"
+  policy = "${file("./policy-DEV-TEAM.hcl")}"
 }
+
